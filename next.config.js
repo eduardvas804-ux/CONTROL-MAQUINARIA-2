52aacdc287b67997d433caae3418f2f71a2215d4
eduardvas804-ux <eduardvas804@gmail.com>
2026-01-22 00:38:25 -0500

    config: add google image domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ['mflvnjdchngasiaafqld.supabase.co'],
+        domains: ['mflvnjdchngasiaafqld.supabase.co', 'lh3.googleusercontent.com'],
     },
 }
 
